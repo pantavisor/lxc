52335706d1975f9c662e5ec18485b8c7d0a7446d
Alexander Sack <asac@pantacor.com>
2020-04-06 14:17:06 +0200

    support resolving absolute symlinks for mount target inside a 'virtual root'.
    This patch implements realpath_x which dirives from freebsd 2013 realpath version to allow for path_resolution
    relative to a sysroot directory. Also this realpath implemntation will not check if final target destination
    exists yet to support for create=file and create=dir.
    
    We use realpath_x for both create file/dir as well as for safe_mount

--- src/lxc/conf.c
+++ src/lxc/conf.c
@@ -53,6 +53,8 @@
 #include "open_utils.h"
 #include "parse.h"
 #include "process_utils.h"
+#include "raw_syscalls.h"
+#include "realpath_x.h"
 #include "ringbuf.h"
 #include "start.h"
 #include "storage/storage.h"
@@ -2639,6 +2641,7 @@ static inline int mount_entry_on_generic(struct mntent *mntent,
 	int ret;
 	bool dev, optional, relative;
 	struct lxc_mount_options opts = {};
+	char realpath[PATH_MAX];
 
 	optional = hasmntopt(mntent, "optional") != NULL;
 	dev = hasmntopt(mntent, "dev") != NULL;
@@ -2647,7 +2650,11 @@ static inline int mount_entry_on_generic(struct mntent *mntent,
 	if (rootfs && rootfs->path)
 		rootfs_path = rootfs->mount;
 
-	ret = mount_entry_create_dir_file(mntent, path, rootfs, lxc_name,
+	/* XXX error handling */
+	realpath_x(rootfs_path, path+strlen(rootfs_path), realpath);
+	INFO("mount_entry_on_generic: path=%s, realpath=%s, rootfs_path=%s, lxc_name=%s, lxc_path=%s", path, realpath, rootfs_path, lxc_name, lxc_path);
+
+	ret = mount_entry_create_dir_file(mntent, realpath, rootfs, lxc_name,
 					  lxc_path);
 	if (ret < 0) {
 		if (optional)
