f27df85bc95c1930dc3195803b429961a317d285
Alexander Sack <asac@pantacor.com>
2020-12-02 17:00:59 +0100

    support /exports mounts

--- src/lxc/conf.c
+++ src/lxc/conf.c
@@ -1530,6 +1530,9 @@ static int lxc_chroot(const struct lxc_rootfs *rootfs)
 			if (strequal(slider1 + 1, "/proc"))
 				continue;
 
+			if (strncmp(slider1 + 1, "/exports", strlen("/exports") * sizeof(char)) == 0)
+				continue;
+
 			ret = umount2(slider1, MNT_DETACH);
 			if (ret == 0)
 				progress++;
@@ -3950,7 +3953,7 @@ static void turn_into_dependent_mounts(const struct lxc_rootfs *rootfs)
 			continue;
 
 		null_endofword(opts);
-		if (!strstr(opts, "shared"))
+		if (!strstr(opts, "shared") || strstr(target, "exports"))
 			continue;
 
 		null_endofword(target);
