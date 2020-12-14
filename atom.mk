98295cbc0a5acd8735e6b37566bfc37ff3d6e221
Alexander Sack <asac@pantacor.com>
2020-12-14 18:42:59 +0100

    fix build failure in buster on x64; --disable-selinux

--- atom.mk
+++ atom.mk
@@ -8,7 +8,7 @@ LOCAL_DESCRIPTION := Linux Containers
 LOCAL_EXPORT_LDLIBS = -l:liblxc.a
 
 LOCAL_AUTOTOOLS_VERSION := 2.0.4
-LOCAL_AUTOTOOLS_CONFIGURE_ARGS := --disable-api --disable-api-docs --enable-static
+LOCAL_AUTOTOOLS_CONFIGURE_ARGS := --disable-api --disable-api-docs --enable-static --disable-selinux
 
 LOCAL_CLEAN_DIRS := $(call local-get-build-dir)/usr/share/ $(call local-get-build-dir)/usr/include/
 
