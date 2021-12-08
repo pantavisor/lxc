4ce293d25ce6864c36447cda14556a3f1458b7d5
Alexander Sack <asac@pantacor.com>
2021-12-09 00:43:31 +0100

    link liblxc dynamically by default

--- atom.mk
+++ atom.mk
@@ -7,7 +7,7 @@ LOCAL_MODULE := lxc
 LOCAL_DESCRIPTION := Linux Containers
 LOCAL_CONDITIONAL_LIBRARIES := OPTIONAL:libseccomp OPTIONAL:apparmor OPTIONAL:libcap
 
-LOCAL_EXPORT_LDLIBS = -l:liblxc.a
+LOCAL_EXPORT_LDLIBS = -llxc
 
 LOCAL_AUTOTOOLS_VERSION := 2.0.4
 LOCAL_AUTOTOOLS_CONFIGURE_ARGS := --disable-api --disable-api-docs --enable-static --disable-selinux
